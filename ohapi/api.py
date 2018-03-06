d9fc415a92fe08947096d398340b0566061484b9
Tarannum Khan <tkhaniitr@gmail.com>
2018-03-06 20:56:02 +0530

    added tests for get_page

--- ohapi/api.py
+++ ohapi/api.py
@@ -113,7 +113,7 @@ def exchange_oauth2_member(access_token):
     return member_data
 
 
-def upload_file(target_filepath, metadata, access_token, project_member_id=None,
+def upload_file(target_filepath, metadata, access_token, project_member_id,
                 remote_file_info=None, base_url=OH_BASE_URL,
                 max_bytes=MAX_FILE_DEFAULT):
     """
@@ -139,10 +139,6 @@ def upload_file(target_filepath, metadata, access_token, project_member_id=None,
 
     logging.info('Uploading {} ({})'.format(
         target_filepath, format_size(filesize)))
-
-    if not(project_member_id):
-        response = exchange_oauth2_member(access_token)
-        project_member_id = response['project_member_id']
 
     r = requests.post(url,
                       files={'data_file': open(target_filepath, 'rb')},
---
+++ ohapi/tests/test_get_page.py
@@ -0,0 +1,30 @@
+import os
+import vcr
+import unittest
+import ohapi.api as api
+
+my_vcr = vcr.VCR(path_transformer=vcr.VCR.ensure_suffix('.yaml'),
+                 cassette_library_dir='ohapi/cassettes', 
+                 filter_query_parameters=['access_token'])
+class test_get_page(unittest.TestCase):
+
+    def setUp(self):
+        self.test_access_token = os.environ.get("access_token",'access_token')
+
+    @my_vcr.use_cassette()
+    def test_get_page_with_results(self):
+        url = ('https://www.openhumans.org/api/direct-sharing/project/'
+                'exchange-member/?access_token={}'.format(self.test_access_token))
+        response = api.get_page(url)
+        self.assertEqual(response['project_member_id'], '40110289')
+        self.assertEqual(response['message_permission'], True)
+        self.assertEqual(response['data'], [])
+        self.assertEqual(response['username'], 'test_user')
+        self.assertEqual(response['sources_shared'], [])
+        self.assertEqual(response['created'],'2018-03-06T05:27:53.995398Z')
+
+    @my_vcr.use_cassette()
+    def test_get_page_invalid_access_token(self):
+        url = ('https://www.openhumans.org/api/direct-sharing/project/'
+                'exchange-member/?access_token={}'.format("invalid_token"))
+        self.assertRaises(Exception,api.get_page,url)  
