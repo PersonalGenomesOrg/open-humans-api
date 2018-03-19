b72d7d2b44d78f250b9c6afc14d265a2c779561a
Tarannum Khan <tkhaniitr@gmail.com>
2018-03-19 11:25:09 +0530

    Added tests for ohproject update_data

--- ohapi/tests/test_projects.py
+++ ohapi/tests/test_projects.py
@@ -1,10 +1,16 @@
 from unittest import TestCase
 from ohapi.projects import OHProject
 
+import pytest
+import vcr
+
 parameter_defaults = {
     'MEMBER_DATA': {"data": [{"basename": 1}]},
     'TARGET_MEMBER_DIR': 'targetmemberdir',
     'MAX_SIZE': 'max_size',
+    'MASTER_ACCESS_TOKEN': 'masteraccesstoken',
+    'MASTER_ACCESS_TOKEN_EXPIRED': 'masteraccesstokenexpired',
+    'MASTER_ACCESS_TOKEN_INVALID': 'masteraccesstokeninvalid',
 }
 
 """
@@ -34,6 +40,15 @@ for param in parameter_defaults:
     locals()[param] = parameter_defaults[param]
 
 
+FILTERSET = [('access_token', 'ACCESSTOKEN')]
+
+my_vcr = vcr.VCR(path_transformer=vcr.VCR.ensure_suffix('.yaml'),
+                 cassette_library_dir='ohapi/cassettes',
+                 filter_headers=[('Authorization', 'XXXXXXXX')],
+                 filter_query_parameters=FILTERSET,
+                 filter_post_data_parameters=FILTERSET)
+
+
 class ProjectsTest(TestCase):
 
     def setUp(self):
@@ -42,3 +57,26 @@ class ProjectsTest(TestCase):
     def test_get_member_file_data_member_data_none(self):
         response = OHProject._get_member_file_data(member_data=MEMBER_DATA)
         self.assertEqual(response, {1: {'basename': 1}})
+
+
+class ProjectsTestUpdateData(TestCase):
+
+    def setUp(self):
+        pass
+
+    @my_vcr.use_cassette()
+    def test_update_data_valid_master_access_token(self):
+        ohproject = OHProject(master_access_token=MASTER_ACCESS_TOKEN)
+        response = ohproject.update_data()
+        self.assertEqual(len(response), 3)
+
+    @my_vcr.use_cassette
+    def test_update_data_expired_master_access_token(self):
+        self.assertRaises(Exception, OHProject, MASTER_ACCESS_TOKEN_EXPIRED)
+
+
+    @my_vcr.use_cassette
+    def test_update_data_invalid_master_access_token(self):
+        self.assertRaises(Exception, OHProject, MASTER_ACCESS_TOKEN_INVALID)
+        # response = ohproject.update_data()
+        # assert response.json() == {"detail": "Invalid token."}
