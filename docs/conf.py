12a0d0b6c830b93f2a6e2484084a97d7222c79d7
Tarannum Khan <tkhaniitr@gmail.com>
2018-03-20 23:00:03 +0530

    Added Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+import mock
+MOCK_MODULES = []
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
@@ -64,7 +67,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','../ohapi/cassettes']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
--- docs/ohapi.rst
+++ docs/ohapi.rst
@@ -1,7 +1,6 @@
 ohapi package
 =============
 
-
 Submodules
 ----------
 
