a5c84c81e20c0009eb36846cee318593cbe0d9bc
Tarannum Khan <tkhaniitr@gmail.com>
2018-05-18 20:32:29 +0530

    added empty case

--- ohapi/utils_fs.py
+++ ohapi/utils_fs.py
@@ -197,6 +197,9 @@ def review_metadata_csv_project_specific(filedir, outputfilepath, subdirs,
         nrows = 0
         for index, row in enumerate(reader):
             nrows = nrows + 1
+            if not row['project_member_id'] or not row['filename'] or
+            not row['tags'] or not row['description']:
+                return False
             if not os.path.isfile(os.path.join(filedir, os.path.join(
                                   str(row['project_member_id'])),
                     row['filename'])):
@@ -231,7 +234,8 @@ def review_metadata_csv_member_specific(filedir, outputfilepath,
         nrows = 0
         for index, row in enumerate(reader):
             nrows = nrows + 1
-            if row['filename'] not in filenames:
+            if row['filename'] not in filenames or not row['filename'] or
+            not row['tags'] or not row['description']:
                 return False
         reader = csv.DictReader(outputfile)
         if len(filenames) != nrows:
